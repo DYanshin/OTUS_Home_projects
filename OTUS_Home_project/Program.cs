629580dac280dfe5eaab984d47be9f120272629c
Denis Yanshin <43885281+DYanshin@users.noreply.github.com>
2025-12-05 01:20:03 +0500

    Update Program.cs

--- OTUS_Home_project/Program.cs
+++ OTUS_Home_project/Program.cs
@@ -7,46 +7,52 @@ namespace first_HomeWork
     {
         static void Main()
         {
-            //!Спросить о возможности рандомного выбора приветствия/взаимодействия с пользователем (выбор из массива?)
-            Console.WriteLine("Добро пожаловать в Бот-трекер цен!");
-
             //Здесь объявляем переменные
             bool progOn = true; // перменная bool при запуске программы
 
             string userName = null; //изначально имя не установленно
+            List<string> userList = new List<string>(); // добавляем список
+
             string userStart = "/start";
             string userHelp = "/help";
             string userInfo = "/info";
             string userExit = "/exit";
             string userEcho = "/echo";
+            string userAddtask = "/addtask"; // Пользователь сможет добавлять задачи в список
+            string userShowtask = "/showtask"; // Пользователь сможет просматривать задачи
+            string userRemovetask = "/removetask"; // пользователь сможет удалить задачи
+
+
+            //!Спросить о возможности рандомного выбора приветствия/взаимодействия с пользователем (выбор из массива?)
+            Console.WriteLine("Добро пожаловать в Бот-трекер цен!");
 
 
             while (progOn)
             {
-                Console.WriteLine("Для продолжения взамодействия введите одну из доступных команд: /start  /help  /info  /exit" + (userName != null ? "  /echo" : "")); //Добавляем /echo в список, если имя установленно
+                Console.WriteLine("Для продолжения взаимодействия введите одну из доступных команд: /start  /help  /info  /addtask  /showtask  /removetask  /exit" + (userName != null ? "  /echo" : "")); //Добавляем /echo в список, если имя установленно
                 var enterCom = Console.ReadLine();
 
                 //Проверяем условия ввода команд
                 if (enterCom == userStart)
                 {
-                    do // обробатываем имя до получения корректного имени
+                    do // обработка имени пользователя до получения корректного имени
                     {
                         Console.WriteLine("Введите свое имя: ");
                         userName = Console.ReadLine()?.Trim(); //Trim() - метод класса string, который удаляет все пробельные символы с начала и конца строки
 
                         if (string.IsNullOrWhiteSpace(userName)) // isNullorWhiteSpace - метод класса string, который проверяет строку на три условия (null, empty, whiteSpace)
                         {
-                            Console.WriteLine("Имя не может быть пустым. Поробуйте снова, вдруг получится?");
+                            Console.WriteLine("Имя не может быть пустым. Попробуйте снова, вдруг получится?");
                         }
                     } while (string.IsNullOrWhiteSpace(userName));
                     Console.WriteLine($"Я запомнил ваше имя, {userName} -_-");
                 }
                 else if (enterCom == userHelp)
                 {
-                    Console.WriteLine("СПРАВОЧНАЯ ИНФОРМАЦИЯ: просто вводить комманды");
+                    Console.WriteLine("СПРАВОЧНАЯ ИНФОРМАЦИЯ: просто вводить комманды из списка");
                     if (userName != null) //Появляется если пользователь ввел свое имя
                     {
-                        Console.WriteLine($"{userName}, внимание! Доступна новая команда: /echo. Введите команду и свой текст");
+                        Console.WriteLine($"{userName}, внимание! Доступна новая команда: /echo. Введите команду и свой текст. Доступны команды /addtask - добавление задачи, /showtasks - показать добавленные задачи и /removetask - удаление задач.");
                     }
                 }
                 else if (enterCom == userInfo)
@@ -82,6 +88,83 @@ namespace first_HomeWork
                         }
                     }
                 }
+
+                // обработка команды /addtask от пользователя
+                else if (enterCom == userAddtask)
+                {
+                    if (userName == null)
+                    {
+                        Console.WriteLine("Неизвестный пользователь, сначала введите команду /start");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{userName}, введите задачу для добавления в список");
+                        string userTask = Console.ReadLine(); // добавляем задачу от пользователя
+                        if (string.IsNullOrWhiteSpace(userTask))
+                        {
+                            Console.WriteLine("Задача не может быть пустой. Введите задачу.");
+                        }
+                        else
+                        {
+                            userList.Add(userTask);
+                            Console.WriteLine($"{userName}, задача добавлена! Всего задач: {userList.Count}");
+                        }
+                    }
+                }
+                // обрабтка команды /showtask 
+                else if (enterCom == userShowtask)
+                {
+                    if (userName == null)
+                    {
+                        Console.WriteLine("Неизвестный пользователь, сначала введите команду /start");
+                    }
+                    else if (userList.Count == 0)
+                    {
+                        Console.WriteLine($"{userName}, у вас нет задач в списке, увы!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{userName}, вот список ваших задач: ");
+                        int counter = 1;
+                        foreach (var userTask in userList)
+                        {
+                            Console.WriteLine($"{counter++}. {userTask}");
+                        }
+                    }
+                }
+                // обработка команды /removetask
+                else if (enterCom == userRemovetask)
+                {
+                    if (userName == null)
+                    {
+                        Console.WriteLine("Неизвестный пользователь, сначала введите команду /start");
+                    }
+                    else if (userList.Count == 0)
+                    {
+                        Console.WriteLine($"{userName}, у вас нет задач для удаления");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{userName}, вот список ваших задач: ");
+                        int counter = 1;
+                        foreach (var task in userList)
+                        {
+                            Console.WriteLine($"{counter++}. {task}");
+                        }
+                        Console.WriteLine($"{userName}, введите порядковый номер задачи д~ля удаления: ");
+                        if (int.TryParse(Console.ReadLine(), out int num) && num >= 1 && num <= userList.Count)
+                        {
+                            userList.RemoveAt(num - 1);
+                            Console.WriteLine($"Успешное удаление задачи {num}!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{userName}, неверный номер задачи на удаление! Введите команду /removetask и попробуйте снова.");
+                        }
+
+                    }
+                }
+
                 else
                 {
                     Console.WriteLine("Упс...неизвестная команда.");
